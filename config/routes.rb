7551487828d447fdcdc3292b16820c7711a9481b
金沢森 <shin.ne1365@gmail.com>
2021-08-05 11:35:59 +0900

    Herokuによるデプロイのための作業 routes.rbの設定〜Gemfileの設定まで

--- config/routes.rb
+++ config/routes.rb
@@ -31,5 +31,7 @@ Rails.application.routes.draw do
   get "books/show" => "books#show"
   post "books/update" => "books#update"
   
+  resources :home
+  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
