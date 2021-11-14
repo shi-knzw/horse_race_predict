acf566cb08d87e701dec4a96f1e59e3ae45bc671
金沢森 <shin.ne1365@gmail.com>
2021-11-14 11:00:22 +0900

    環境変数の書式を修正

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,14 +1,14 @@
 RakutenWebService.configure do |c|
     # (必須) アプリケーションID
     if Rails.env.production?
-      c.application_id = ENV["application_id"]
+      c.application_id = ENV["APPLICATION_ID"]
     else
       c.application_id = Settings.rakuten[:application_id]
     end
 
     # (任意) 楽天アフィリエイトID
     if Rails.env.production?
-      c.application_id = ENV["affiliate_id"]
+      c.application_id = ENV["APPLICATON_ID"]
     else
       c.affiliate_id = Settings.rakuten[:affiliate_id] # default: nil
     end
